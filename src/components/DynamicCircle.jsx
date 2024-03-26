805e8031227d734d9c708a4f26bc73da0d8d371b
Paolo <pra@alternateeve.com>
2024-03-26 18:32:38 +0300

    Animation texte avec tiret

--- src/components/DynamicCircle.jsx
+++ src/components/DynamicCircle.jsx
@@ -6,6 +6,7 @@ const DynamicCircle = () => {
     <nav className="absolute right-5 top-[50%] translate-y-1/2 h-fit z-[13]">
       <div class="conteneur">
         <div class="cercle"></div>
+        <div class="texte">Texte à afficher</div>
       </div>
     </nav>
   );
