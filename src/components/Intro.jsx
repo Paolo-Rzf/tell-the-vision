fecb1104c5d4ecc84c67e5262b38af17e6bd4d47
Paolo <paolorandry@gmail.com>
2024-03-30 15:53:15 +0300

    Logo intronisation

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Intro = ({ onStart }) => {
   return (
     <div className='h-screen w-screen flex justify-center items-center cursor-pointer'>
-      <img src='icons/intro.svg' onClick={onStart} className='h-[90vh]' />
+      <img src='images/intro.png' onClick={onStart} className='h-[90vh]' />
     </div>
   )
 }
